26ccefdd64972b9e1369423cdf80f428e69d6ef1
russell hofvendahl <rhofvendahl@gmail.com>
2018-03-22 16:37:01 -0700

    add cleanup plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +9,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  plugins: [
+    new CleanWebpackPlugin(['dist']),   // new line
     new HtmlWebpackPlugin({
       title: 'Ping Pong',
       template: './src/index.html',
