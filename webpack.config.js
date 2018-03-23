5e7e9c57823ef5eed4d1e255e11d6d6e597701c3
russell hofvendahl <rhofvendahl@gmail.com>
2018-03-22 19:25:15 -0700

    fix copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
